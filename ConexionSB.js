2ae76cb89a83b88669bced6f377fb633bd10c047
Snayder Mucha <snayderronaldinho09@gmail.com>
2025-11-18 23:03:33 -0500

    primer commit

---
+++ ConexionSB.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://nidbmvvfdcjpkukzmshq.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pZGJtdnZmZGNqcGt1a3ptc2hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyNDE1MDgsImV4cCI6MjA3ODgxNzUwOH0.ovp5jZu8yfZqDr-GqNxsjQg1Al5sXmWZbD6_FDLNPwo"
+);
